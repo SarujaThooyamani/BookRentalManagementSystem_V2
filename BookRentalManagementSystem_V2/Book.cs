1b0f1276ca54b349677ab47e6d618da664539816
UT01372 <zarasaru07@gmail.com>
2024-10-11 15:50:16 +0530

    11qs

---
+++ BookRentalManagementSystem_V2/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V2
+{
+    internal class Book
+    {
+        public string Bookid {  get; set; }
+        public string Title {  get; set; }
+        public string Author{  get; set; }
+        public decimal RentalPrice {  get; set; }
+
+    }
+}
